394d8e025e6ede88659976366abb682c2aaed579
whitekiteoversea <754641132@qq.com>
2024-06-01 15:09:46 +0800

    NEW: 首次提交，部分待验证

---
+++ ADclear.bat
@@ -0,0 +1,11 @@
+del *.SchDoc.Zip		/s
+del *.PcbDoc.Zip		/s
+del *.SchLib.Zip		/s
+del *.PcbLib.Zip		/s
+del *.pcb.Zip			/s
+del *.PrjPCB.Zip		/s
+del *.pcbdoc_viewstate	/s
+del *.SchDocPreview		/s
+del *.PcbDocPreview		/s
+del *.$$$Preview        /s
+del *.LOG				/s
